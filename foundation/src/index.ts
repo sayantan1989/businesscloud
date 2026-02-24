f7e20e2016e4688521ff2fa747194a39f9ffed3d
I316810 <s.chaudhury@sap.com>
2026-02-24 13:16:08 +0530

    poc-2

--- foundation/src/index.ts
+++ foundation/src/index.ts
@@ -1,4 +1,16 @@
+import express from "express";
 import _ from "lodash";
+import minimist from "minimist";
 
-console.log("Mend test repo");
-console.log(_.chunk([1,2,3,4], 2));
+const app = express();
+
+const args = minimist(process.argv.slice(2));
+
+app.get("/", (req, res) => {
+    const chunked = _.chunk([1,2,3,4], 2);
+    res.send(`Args: ${JSON.stringify(args)} Chunked: ${JSON.stringify(chunked)}`);
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
